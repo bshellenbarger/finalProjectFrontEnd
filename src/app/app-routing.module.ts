b9c9c7a0ee5c680afe647c845260c3cc59ce1290
bshellenbarger <brittney.shellenbarger@gmail.com>
2021-03-08 21:48:47 -0500

    Revert "all running now"
    This reverts commit cec2112816e7a577314ad51677c4040027222db2.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-// import { AddVoiceComponent } from './add-voice/add-voice.component';
+import { AddVoiceComponent } from './add-voice/add-voice.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
   { path: '', component: DashboardComponent },
   { path: 'dashboard', component: DashboardComponent },
-  // { path: 'addVoice', component: AddVoiceComponent }
+  { path: 'addVoice', component: AddVoiceComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { AddVoiceComponent } from './add-voice/add-voice.component';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
@@ -12,6 +13,7 @@ import { HttpClientModule } from '@angular/common/http';
     AppComponent,
     HeaderComponent,
     DashboardComponent,
+    AddVoiceComponent
   ],
   imports: [
     BrowserModule,
